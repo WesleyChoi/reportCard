977b2f0527925f84eca0e6b01b09736fa9f9d994
Wesley <wesleyhchoi@gmail.com>
2022-06-06 15:42:32 -0700

    progress on front end UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tasks = [
+  {
+    id: 1,
+    title: "test one",
+    description: "bla bla bla",
+    completed: false
+  },
+  {
+    id: 2,
+    title: "test two",
+    description: "bla bla bla",
+    completed: true
+  },
+  {
+    id: 3,
+    title: "test three",
+    description: "bla bla bla",
+    completed: false
+  },
+]
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      taskList: tasks,
+    };
+  }
+
+  displayCompleted = status => {
+    if (status) {
+      return this.setstatus({viewCompleted: true});
+    }
+    return this.setstatus({viewCompleted: false})
+  }
+
+  renderTabList = () => {
+    return(
+      <div className="my-5 tab-list">
+        <span onClick={() => this.displayCompleted(true)} className={this.state.viewCompleted ? "active" : ""}>Completed</span>
+        <span onClick={() => this.displayCompleted(false)} className={this.state.viewCompleted ? "" : "active"}>Incompleted</span>
+      </div>
+    )
+  };
+
+  renderItems = () => {
+    const {viewCompleted } = this.state;
+    const newItems = this.state.taskList.filter(
+      item => item.completed == viewCompleted
+    );
+    return newItems.map(item => (
+      <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
+        <span className={`todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""}`} title={item.title}>
+          {item.title}
+        </span>
+        <span>
+          <button className="btn btn-info mr-2">Edit</button>
+          <button className="btn btn-danger mr-2">Delete</button>
+        </span>
+      </li>
+    ))
+  };
+
+  render() {
+    return(
+      <main className="context">
+        <h1 className="text-black text-uppercase text-center my-4">Task Manager</h1>
+        <div className="row">
+          <div className="coll-md-6 col-sma-10 mx-auto p-0">
+            <div className="card p-3">
+              <div>
+                <button className="btn btn-primary">Add Task</button>
+              </div>
+              {this.renderTabList()}
+              <ul className="list-group list-group-flush">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+    )
+  }
+
+
 }
 
 export default App;
